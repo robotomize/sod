a95cb5fdad402d494047d2a97dfdfaa357572360
robotomize <robotomize@gmail.com>
2020-10-19 11:14:29 +1100

    added test TestDbxExecutorFlusher

--- internal/outlier/db_tx_executor_test.go
+++ internal/outlier/db_tx_executor_test.go
@@ -2,11 +2,72 @@ package outlier
 
 import (
 	"context"
+	"errors"
 	"sod/internal/geom"
 	"sod/internal/metric/model"
+	"sync/atomic"
 	"testing"
 	"time"
 )
+
+func TestDbxExecutorFlusher(t *testing.T) {
+	tests := []struct {
+		name           string
+		txExecutor     *dbTxExecutor
+		shutdownCh     chan error
+		expectedErr    error
+		expectedLen    int
+		expectedBufLen int
+		waitingTime    time.Duration
+		batch          []model.Metric
+	}{
+		{
+			name:        "positive_shutdown",
+			waitingTime: 1 * time.Second,
+			txExecutor:  &dbTxExecutor{opts: dbTxExecutorOptions{dbFlushTime: 1 * time.Second}},
+			batch: []model.Metric{
+				model.NewMetric("test-data", geom.Point{1, 1, 1, 1}, time.Now(), "test"),
+				model.NewMetric("test-data", geom.Point{1, 1, 1, 1}, time.Now(), "test"),
+				model.NewMetric("test-data", geom.Point{1, 1, 1, 1}, time.Now(), "test"),
+				model.NewMetric("test-data", geom.Point{1, 1, 1, 1}, time.Now(), "test"),
+				model.NewMetric("test-data", geom.Point{1, 1, 1, 1}, time.Now(), "test"),
+			},
+			expectedLen:    5,
+			expectedBufLen: 0,
+			expectedErr:    nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			length := 0
+			bit := int64(0)
+			ctx, cancel := context.WithCancel(context.TODO())
+			test.txExecutor.buf = test.batch
+			go test.txExecutor.flusher(ctx, func(ctx context.Context, metrics []model.Metric) error {
+				if atomic.LoadInt64(&bit) == 0 {
+					length = len(metrics)
+				} else {
+					atomic.StoreInt64(&bit, 1)
+				}
+
+				return nil
+			})
+
+			time.Sleep(test.waitingTime)
+			cancel()
+			if length != test.expectedLen {
+				t.Errorf(
+					"calling the shutdown method, the length of the inserted data got: %v, expected: %v",
+					length, test.expectedLen)
+			}
+			if len(test.txExecutor.buf) != test.expectedBufLen {
+				t.Errorf(
+					"calling the shutdown method, the length of buffer got: %v, expected: %v",
+					len(test.txExecutor.buf), test.expectedBufLen)
+			}
+		})
+	}
+}
 
 func TestDbTxExecutorAppend(t *testing.T) {
 	tests := []struct {
@@ -101,9 +162,9 @@ func TestDbTxExecutorShutdown(t *testing.T) {
 		name           string
 		txExecutor     *dbTxExecutor
 		shutdownCh     chan error
-		expectedErr    error
 		expectedLen    int
 		expectedBufLen int
+		expectedErr    error
 	}{
 		{
 			name: "positive_shutdown",
@@ -127,7 +188,7 @@ func TestDbTxExecutorShutdown(t *testing.T) {
 			},
 			expectedLen:    0,
 			expectedBufLen: 0,
-			expectedErr:    nil,
+			expectedErr:    errors.New("test"),
 		},
 	}
 	for _, test := range tests {
@@ -136,9 +197,12 @@ func TestDbTxExecutorShutdown(t *testing.T) {
 
 			err := test.txExecutor.shutdown(func(ctx context.Context, metrics []model.Metric) error {
 				length = len(metrics)
+				if test.expectedErr != nil {
+					return test.expectedErr
+				}
 				return nil
 			})
-			if err != test.expectedErr {
+			if test.expectedErr == nil && err != nil {
 				t.Errorf("calling the shutdown method, err got: %v, expected: %v", err, test.expectedErr)
 			}
 			if length != test.expectedLen {
